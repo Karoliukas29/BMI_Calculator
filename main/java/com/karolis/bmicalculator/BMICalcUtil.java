aeb1787a931b40028c543402ae30507ff96f1f1f
Karoliukas29 <77672126+Karoliukas29@users.noreply.github.com>
2021-05-10 16:07:24 +0100

    new file added

---
+++ main/java/com/karolis/bmicalculator/BMICalcUtil.java
@@ -0,0 +1,39 @@
+package com.karolis.bmicalculator;
+
+public class BMICalcUtil {
+    public static final BMICalcUtil instance = new BMICalcUtil();
+
+    private static final int CENTIMETERS_IN_METER = 100;
+    private static final int INCHES_IN_FOOT = 12;
+    private static final int BMI_IMPERIAL_WEIGHT_SCALAR = 703;
+
+    public static final String BMI_CATEGORY_UNDERWEIGHT = "Your BMI is less than 18.5, that indicates that you are underweight. You are recommended to see your doctor for advice";
+    public static final String BMI_CATEGORY_HEALTHY = "Your BMI is at Healthy Weight Range,it indicates that you are at a healthy weight for your height!";
+    public static final String BMI_CATEGORY_OVERWEIGHT = "A BMI of 25-29.9 indicates that you are slightly overweight, in order to avoid health issues in the future speak with your doctor";
+    public static final String BMI_CATEGORY_OBESE = "A BMI over 30 indicates that you are heavily overweight. Your health might be at risk if you won't lose some weight. You are highly recommended to see doctor for advice";
+
+    public static BMICalcUtil getInstance() {
+        return instance;
+    }
+
+    public double calculateBMIMetric(double heightCm, double weightKg) {
+        return (weightKg / ((heightCm / CENTIMETERS_IN_METER) * (heightCm / CENTIMETERS_IN_METER)));
+    }
+
+    public double calculateBMIImperial(double heightFeet, double heightInches, double weightLbs) {
+        double totalHeightInInches = (heightFeet * INCHES_IN_FOOT) + heightInches;
+        return (BMI_IMPERIAL_WEIGHT_SCALAR * weightLbs) / (totalHeightInInches * totalHeightInInches);
+    }
+
+    public String classifyBMI(double bmi) {
+        if (bmi < 18.5) {
+            return BMI_CATEGORY_UNDERWEIGHT;
+        } else if (bmi >= 18.5 && bmi < 25) {
+            return BMI_CATEGORY_HEALTHY;
+        } else if (bmi >= 25 && bmi < 30){
+            return BMI_CATEGORY_OVERWEIGHT;
+        } else {
+            return BMI_CATEGORY_OBESE;
+        }
+    }
+}
